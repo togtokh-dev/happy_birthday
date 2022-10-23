454d3d8c32f0c74b5ab06a00aa014d871be7c6c4
DESKTOP-79JTF78\TG <togtokh.dev@gmail.com>
2022-10-23 12:34:57 +0800

    main

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
+<?php 
+  $actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; date_default_timezone_set("Asia/Ulaanbaatar");
+  $name = "";
+  if(isset($_GET['name'])){
+    $name = $_GET['name'];
+  }
+?>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>(ТӨРСӨН ӨДРИЙН МЭНД)</title>
-    <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+    <meta property="og:url"                content="<?php echo $actual_link; ?>" />
     <meta property="og:type"               content="article" />
     <meta property="og:title"              content="(ТӨРСӨН ӨДРИЙН МЭНД)" />
     <meta property="og:description"        content="(ТӨРСӨН ӨДРИЙН МЭНД)" />
-    <meta property="og:image"              content="./banner.png" />
+    <meta property="og:image"              content="<?php echo $actual_link; ?>/banner.png" />
     <link rel="stylesheet" href="./style.css">
-    <link rel="icon" href="./favicon.ico" />
+    <link rel="icon" href="<?php echo $actual_link; ?>/favicon.ico" />
   </head>
   <body>
     <div class="card">
@@ -22,7 +29,7 @@
       <div class="details">
         <!-- <h4 class="color1">Munkhbayar Munkhbat</h2> -->
         <h4 class="color2 margin">(ТӨРСӨН ӨДРИЙН МЭНД)</h3>
-          <p>Эрхэм  аа,</p>
+          <p>Эрхэм <?php echo $name; ?> аа,</p>
           <p>...</p>
           <p>Амьдрал чинь аз жаргалаар дүүрэн</p>
           <p>байг</p>
@@ -32,7 +39,7 @@
           <p>байг</p>
           <p>Хүссэн бүхэн чинь биелэх болтугай</p>
           <p class="text-right">Төрсөн өдрийн мэнд хүргэе,</p>
-          <p>аа!</p>
+          <p><?php echo $name; ?> аа!</p>
           <p class="text-right">♥Togtokhoo</p>
       </div>
     </div>
